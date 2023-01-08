23adfe302b03f155a172beae6571f19cbe5f9dde
Stabilizer360 <stabilizergaming27@gmail.com>
2023-01-08 15:24:17 +0500

    Update 3.0

--- src/main/java/com/stabilizerking/stabxmodernguns/registeration/ModItemRegisteration.java
+++ src/main/java/com/stabilizerking/stabxmodernguns/registeration/ModItemRegisteration.java
@@ -7,6 +7,7 @@ import com.mrcrayfish.guns.item.attachment.impl.Barrel;
 import com.mrcrayfish.guns.item.attachment.impl.Scope;
 import com.mrcrayfish.guns.item.attachment.impl.Stock;
 import com.mrcrayfish.guns.item.attachment.impl.UnderBarrel;
+import com.stabilizerking.stabxmodernguns.item.M1GrandeGunItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,56 +19,83 @@ public class ModItemRegisteration {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, StabxModernGuns.MOD_ID);
     public static final Item.Properties genericProperties = new Item.Properties().stacksTo(1).tab(StabxModernGuns.GROUP);
 
-    //Guns in the Modern Mod
+    //Guns and Everything
 
-    public static final RegistryObject<Item> FIVE_ZERO_ZERO_MAGNUM_SMITH_AND_WESSON = registerGun("500magnum_smith_and_wesson", false);
+    //Pistols
+    public static final RegistryObject<Item> FIVE_HUNDRED_MAGNUM_SMITH_AND_WESSON = registerGun("500magnum_smith_and_wesson", false);
+    public static final RegistryObject<Item> GLOCK_19C= registerGun("glock19c", false);
+    public static final RegistryObject<Item> DESERT_EAGLE_MARK_14= registerGun("desert_eagle_mark14", false);
 
-    public static final RegistryObject<Item> GLOCK_ONE_NINE_C= registerGun("glock19c", false);
-    public static final RegistryObject<Item> HK_FOUR_ONE_SIX= registerGun("hk416", false);
-    public static final RegistryObject<Item> MK_MOD_ONE_ONE= registerGun("mkmod11", false);
-    public static final RegistryObject<Item> SCAR_ONE_SEVEN= registerGun("scar17", false);
-    public static final RegistryObject<Item> AK_FOUR_SEVEN= registerGun("ak47", false);
-    public static final RegistryObject<Item> AKS_SEVENTY_FOUR_UX= registerGun("aks74ux", false);
-    public static final RegistryObject<Item> STG_FOUR_FOUR= registerGun("stg44", false);
-    public static final RegistryObject<Item> MP_FIVE= registerGun("mp5_classic", false);
-    public static final RegistryObject<Item> M_SIXTEEN= registerGun("m16", false);
+    //SMGs
+    public static final RegistryObject<Item> MP_5_CLASSIC= registerGun("mp5_classic", false);
+    public static final RegistryObject<Item> STRIKER_45= registerGun("striker45", false);
+    public static final RegistryObject<Item> TEC_9= registerGun("tec9", false);
+    public static final RegistryObject<Item> MP40= registerGun("mp40", false);
+
+
+    //Assault Rifles
     public static final RegistryObject<Item> M_FOUR= registerGun("m4", false);
-    public static final RegistryObject<Item> G_THIRTYSIX_CHD= registerGun("g36chd", false);
-    public static final RegistryObject<Item> M_FOUR_A_ONE= registerGun("m4a1", false);
-    public static final RegistryObject<Item> SPAS_ONE_TWO= registerGun("spas12", false);
-    public static final RegistryObject<Item> ARISAKA_TYPE_NINE_NINE= registerGun("arisaka_type99", false);
-    public static final RegistryObject<Item> ARTIC_WARFARE_MAGNUM= registerGun("artic_warfare_magnum", false);
-    public static final RegistryObject<Item> AA_TWELVE= registerGun("aa12", false);
-    public static final RegistryObject<Item> STRIKER_FOURTY_FIVE= registerGun("striker45", false);
-    public static final RegistryObject<Item> AKM_SEVEN_SIXTYTWO= registerGun("akm_762", false);
-    public static final RegistryObject<Item> M_SIXTY= registerGun("m60", false);
-    public static final RegistryObject<Item> DESERT_EAGLE_MARK_FOURTEEN= registerGun("desert_eagle_mark14", true);
+    public static final RegistryObject<Item> HK_416= registerGun("hk416", false);
+    public static final RegistryObject<Item> MK_MOD_11= registerGun("mkmod11", false);
+    public static final RegistryObject<Item> SCAR_17= registerGun("scar17", false);
+    public static final RegistryObject<Item> AK47= registerGun("ak47", false);
+    public static final RegistryObject<Item> AKM_762= registerGun("akm_762", false);
+    public static final RegistryObject<Item> AKS_74UX= registerGun("aks74ux", false);
+    public static final RegistryObject<Item> STG_44= registerGun("stg44", false);
+    public static final RegistryObject<Item> ACR_BUSHMASTER= registerGun("acr_360_bushmaster", false);
+    public static final RegistryObject<Item> M16= registerGun("m16", false);
+    public static final RegistryObject<Item> M4A1= registerGun("m4a1", false);
+    public static final RegistryObject<Item> G_36C_HD= registerGun("g36chd", false);
 
+    //Rifles
+    public static final RegistryObject<Item> ARISAKA_TYPE99= registerGun("arisaka_type99", false);
+    public static final RegistryObject<M1GrandeGunItem> M_ONE_GRANDE = ITEMS.register("m1_grande",
+            () -> new M1GrandeGunItem(new Item.Properties().stacksTo(1).tab(StabxModernGuns.GROUP)));
+
+
+    //Sniper Rifles
+    public static final RegistryObject<Item> ARTIC_WARFARE_MAGNUM= registerGun("artic_warfare_magnum", false);
+    public static final RegistryObject<Item> BARRET_M82_SNIPER= registerGun("barrett_m82_sniper", false);
+
+    //Shotguns
+    public static final RegistryObject<Item> SPAS_12= registerGun("spas12", false);
+    public static final RegistryObject<Item> AA_12= registerGun("aa12", false);
+
+    //Light Machine Guns
+    public static final RegistryObject<Item> M_60= registerGun("m60", false);
+
+    //Mics other Guns
+    public static final RegistryObject<Item> MK_170_CROSSBOW= registerGun("mk_170_crossbow", false);
 
     // Sights Attachments
     public static final RegistryObject<Item> ZA_MINI_RED_DOT_SIGHT = ITEMS.register("za_mini_red_dot_sight", () -> new ScopeItem(Scope.create(0.1F, 1.51F, GunModifiers.SLOW_ADS).viewFinderOffset(0.5), genericProperties, false));
 
-   // Gun Stock
+    // Gun Stock
     public static final RegistryObject<Item> CUSTOM_STOCK = ITEMS.register("custom_stock", () -> new StockItem(Stock.create(GunModifiers.STABILISED), genericProperties, false));
 
     //Gun Barrel Attachments
     public static final RegistryObject<Item> ADVANCED_SUPPRESSOR = ITEMS.register("advanced_suppressor", () -> new BarrelItem(Barrel.create(17.0f, GunModifiers.SILENCED), genericProperties));
 
-    //ForeGrips And Under barrel attachments
+    //Under barrel attachments
     public static final RegistryObject<Item> ANGLED_FOREGRIP = ITEMS.register("angled_foregrip", () -> new UnderBarrelItem(UnderBarrel.create(GunModifiers.BETTER_CONTROL), genericProperties));
 
 
     //Weaapons Ammo
     public static final RegistryObject<Item> NINE_MM = registerAmmo("9mm");
+    public static final RegistryObject<Item> NINE_X_NINTEEN_MM_PARABELLUM = registerAmmo("9x19mm_parabellum");
     public static final RegistryObject<Item> SEVEN_SIXTYTWO_MM = registerAmmo("7.62mm");
-    public static final RegistryObject<Item> TOWEVLE_GUAGE_BUCKSHOT = registerAmmo("12gauge_buckshot_slug_shell_360");
+    public static final RegistryObject<Item> SEVEN_SIXTY_TWO_X_FIFTYONE_MM_NATO= registerAmmo("7.62x51mm_nato");
+    public static final RegistryObject<Item> TWEVLE_GAUGE_BUCKSHOT_SLUG = registerAmmo("12gauge_buckshot_slug_shell_360");
     public static final RegistryObject<Item> FIVE_FIFTYSIX_X_FOURTY_FIVE_NATO = registerAmmo("5.56x45_nato");
     public static final RegistryObject<Item> SEVEN_SEVEN_X_FIFTY_EIGHT_MM = registerAmmo("7.7x58mm");
     public static final RegistryObject<Item> SEVEN_THIRTYTWO_MM_KURZ = registerAmmo("7.32x33mm_kurz");
     public static final RegistryObject<Item> FIFTY_CALIBER = registerAmmo("50_caliber");
     public static final RegistryObject<Item> THREE_HUNDRED_WM = registerAmmo("300wm");
     public static final RegistryObject<Item> FIFTY_AE = registerAmmo("50ae");
-    public static final RegistryObject<Item> FOURTYFIVE_ACP = registerAmmo("45acp");
+    public static final RegistryObject<Item> FOURTY_FIVE_ACP = registerAmmo("45acp");
+    public static final RegistryObject<Item> THIRTY_SIXTY_SPRINGFIELD_CARTRIDGE = registerAmmo("30_60_springfield_cartridge");
+    public static final RegistryObject<Item> FIFTY_BMG = registerAmmo("50bmg");
+    public static final RegistryObject<Item> MK_CROSSBOW_ARROW= registerAmmo("mk_crossbow_arrow");
 
 
    //Just a helper Methid used here for gun coloring
--- src/main/java/com/stabilizerking/stabxmodernguns/registeration/ModSoundRegisteration.java
+++ src/main/java/com/stabilizerking/stabxmodernguns/registeration/ModSoundRegisteration.java
@@ -15,23 +15,30 @@ public class ModSoundRegisteration {
     // Gun Sounds
 
     //Pistols Gun Sounds
-    public static final RegistryObject<SoundEvent> GLOCK_NINTEEN_C_FIRE = register("item.glock19c.fire");
-    public static final RegistryObject<SoundEvent> GLOCK_NINTEEN_C_SUPPRESSED = register("item.glock19c.suppressed");
-    public static final RegistryObject<SoundEvent> MAGNUM_FIVE_HUNDREDED_FIRE = register("item.magnum500.fire");
-    public static final RegistryObject<SoundEvent> MAGNUM_FIVE_HUNDREDED_SUPPRESSED = register("item.magnum500.suppressed");
-    public static final RegistryObject<SoundEvent> DESERT_EAGLE_MARK_FOURTEEN_FIRE = register("item.desert_eagle_mark14.fire");
+    public static final RegistryObject<SoundEvent> GLOCK_19C_FIRE = register("item.glock19c.fire");
+    public static final RegistryObject<SoundEvent> GLOCK_19C_SUPPRESSED = register("item.glock19c.suppressed");
+    public static final RegistryObject<SoundEvent> MAGNUM_500_FIRE = register("item.magnum500.fire");
+    public static final RegistryObject<SoundEvent> MAGNUM_500_SUPPRESSED = register("item.magnum500.suppressed");
+    public static final RegistryObject<SoundEvent> DESERT_EAGLE_MARK_14_FIRE = register("item.desert_eagle_mark14.fire");
 
     //SMGs Gun sounds
-    public static final RegistryObject<SoundEvent> MP_FIVE_CLASSIC_FIRE = register("item.mp5_classic.fire");
-    public static final RegistryObject<SoundEvent> MP_FIVE_CLASSIC_COCK = register("item.mp5_classic.cock");
-    public static final RegistryObject<SoundEvent> STRIKER_FOURTY_FIVE_FIRE = register("item.striker45.fire");
-    public static final RegistryObject<SoundEvent> STRIKER_FOURTY_FIVE_COCK = register("item.striker45.cock");
+    public static final RegistryObject<SoundEvent> MP5_CLASSIC_FIRE = register("item.mp5_classic.fire");
+    public static final RegistryObject<SoundEvent> MP5_CLASSIC_COCK = register("item.mp5_classic.cock");
+    public static final RegistryObject<SoundEvent> STRIKER45_FIRE = register("item.striker45.fire");
+    public static final RegistryObject<SoundEvent> STRIKER45_COCK = register("item.striker45.cock");
+    public static final RegistryObject<SoundEvent> MP40_FIRE = register("item.mp40.fire");
+    public static final RegistryObject<SoundEvent> MP40_COCK = register("item.mp40.cock");
+    public static final RegistryObject<SoundEvent> TEC9_FIRE = register("item.tec9.fire");
 
     //Assault Rifles Gun Sounds
-    public static final RegistryObject<SoundEvent> AK_FOURTY_SEVEN_FIRE = register("item.ak47.fire");
-    public static final RegistryObject<SoundEvent> AK_FOURTY_SEVEN_COCK = register("item.ak47.cock");
-    public static final RegistryObject<SoundEvent> AK_FOURTY_SEVEN_SUPPRESSED = register("item.ak47.suppressed");
-    public static final RegistryObject<SoundEvent> HK_FOUR_ONE_SIX_FIRE  = register("item.hk416.fire");
+    public static final RegistryObject<SoundEvent> AK47_FIRE = register("item.ak47.fire");
+    public static final RegistryObject<SoundEvent> AK47_COCK = register("item.ak47.cock");
+    public static final RegistryObject<SoundEvent> AK47_SUPPRESSED = register("item.ak47.suppressed");
+    public static final RegistryObject<SoundEvent> AKM_762_FIRE = register("item.akm762.fire");
+    public static final RegistryObject<SoundEvent> AKM_762_COCK = register("item.akm762.cock");
+    public static final RegistryObject<SoundEvent> AKS_74UX_FIRE = register("item.aks74ux.fire");;
+    public static final RegistryObject<SoundEvent> AKS_74UX_COCK = register("item.aks74ux.cock");
+    public static final RegistryObject<SoundEvent> HK416_SIX_FIRE  = register("item.hk416.fire");
     public static final RegistryObject<SoundEvent> STG_FOURTY_FOUR_FIRE = register("item.stg44.fire");
     public static final RegistryObject<SoundEvent> STG_FOURTY_FOUR_SUPPRESSED = register("item.stg44.suppressed");
     public static final RegistryObject<SoundEvent> SCAR_SEVENTEEN_FIRE = register("item.scar17.fire");
@@ -40,37 +47,51 @@ public class ModSoundRegisteration {
     public static final RegistryObject<SoundEvent> MK_MOD_ELEVEN_SUPPRESSED = register("item.mkmod11.suppressed");
     public static final RegistryObject<SoundEvent> MK_MOD_ELEVEN_RELOAD = register("item.mkmod11.reload");
     public static final RegistryObject<SoundEvent> MK_MOD_ELEVEN_COCK = register("item.mkmod11.cock");
-    public static final RegistryObject<SoundEvent> G_THIRTY_SIX_CHD_FIRE = register("item.g36chd.fire");
-    public static final RegistryObject<SoundEvent> G_THIRTY_SIX_CHD_SUPPRESSED = register("item.g36chd.suppressed");
-    public static final RegistryObject<SoundEvent> G_THIRTY_SIX_CHD_COCK = register("item.g36chd.cock");
     public static final RegistryObject<SoundEvent> M_FOUR_FIRE = register("item.m4.fire");
     public static final RegistryObject<SoundEvent> M_FOUR_COCK = register("item.m4.cock");
-    public static final RegistryObject<SoundEvent> M_SIXTEEN_FIRE = register("item.m16.fire");
-    public static final RegistryObject<SoundEvent> M_SIXTEEN_SUPPRESSED = register("item.m16.suppressed");
-    public static final RegistryObject<SoundEvent> M_SIXTEEN_COCK = register("item.m16.cock");
     public static final RegistryObject<SoundEvent> M_FOUR_A_ONE_FIRE = register("item.m4a1.fire");
-    public static final RegistryObject<SoundEvent> M_FOUR_A_ONE_SUPPRESSED = register("item.m4a1.suppressed");
     public static final RegistryObject<SoundEvent> M_FOUR_A_ONE_COCK = register("item.m4a1.cock");
+    public static final RegistryObject<SoundEvent> M_FOUR_A_ONE_SUPPRESSED = register("item.m4a1.suppressed");
+    public static final RegistryObject<SoundEvent> M_SIXTEEN_FIRE = register("item.m16.fire");
+    public static final RegistryObject<SoundEvent> M_SIXTEEN_COCK = register("item.m16.cock");
+    public static final RegistryObject<SoundEvent> M_SIXTEEN_SUPPRESSED = register("item.m16.suppressed");
+    public static final RegistryObject<SoundEvent> G_THIRTYSIX_C_HD_FIRE = register("item.g36chd.fire");
+    public static final RegistryObject<SoundEvent> G_THIRTYSIX_C_HD_COCK = register("item.g36chd.cock");
+    public static final RegistryObject<SoundEvent> G_THIRTYSIX_C_HD_SUPPRESSED = register("item.g36chd.suppressed");
+    public static final RegistryObject<SoundEvent> ACR_BUSHMASTER_FIRE = register("item.acr_bushmaster.fire");
+    public static final RegistryObject<SoundEvent> ACR_BUSHMASTER_COCK = register("item.acr_bushmaster.cock");
+
 
     //LMG Gun sounds
-    public static final RegistryObject<SoundEvent> M_SIXTY_FIRE = register("item.m60.fire");
-    public static final RegistryObject<SoundEvent> M_SIXTY_SUPPRESSED = register("item.m60.suppressed");
+    public static final RegistryObject<SoundEvent> M60_FIRE = register("item.m60.fire");
+    public static final RegistryObject<SoundEvent> M60_SUPPRESSED = register("item.m60.suppressed");
 
 
     //Rifles Gun Sounds
-    public static final RegistryObject<SoundEvent> ARISAKA_TYPE_NINETY_NINE_FIRE = register("item.arisaka_type99.fire");
-    public static final RegistryObject<SoundEvent> ARISAKA_TYPE_NINETY_NINE_COCK = register("item.arisaka_type99.cock");
+    public static final RegistryObject<SoundEvent> ARISAKA_TYPE_99_FIRE = register("item.arisaka_type99.fire");
+    public static final RegistryObject<SoundEvent> ARISAKA_TYPE_99_COCK = register("item.arisaka_type99.cock");
+
+    public static final RegistryObject<SoundEvent> M1_GRANDE_FIRE = register("item.m1_grande.fire");
+    public static final RegistryObject<SoundEvent> M1_GRANDE_COCK = register("item.m1_grande.cock");
+    public static final RegistryObject<SoundEvent> M1_GRANDE_PING = register("item.m1_grande.ping");
+
     //Sniper Rifles Gun Sounds
     public static final RegistryObject<SoundEvent> ARTIC_WARFARE_MAGNUM_FIRE = register("item.artic_warfare_magnum.fire");
     public static final RegistryObject<SoundEvent> ARTIC_WARFARE_MAGNUM_COCK = register("item.artic_warfare_magnum.cock");
+    public static final RegistryObject<SoundEvent> M82_FIRE = register("item.m82.fire");
+    public static final RegistryObject<SoundEvent> M82_COCK = register("item.m82.cock");
 
     //Shotgun Gun Sounds
-    public static final RegistryObject<SoundEvent> SPAS_TWELVE_FIRE = register("item.spas12.fire");
-    public static final RegistryObject<SoundEvent> SPAS_TWELVE_COCK = register("item.spas12.cock");
-    public static final RegistryObject<SoundEvent> SPAS_TWELVE_RELOAD = register("item.spas12.reload");
-    public static final RegistryObject<SoundEvent> AA_TWELVE_FIRE = register("item.aa12.fire");
-    public static final RegistryObject<SoundEvent> AA_TWELVE_COCK = register("item.aa12.cock");
-    public static final RegistryObject<SoundEvent> AA_TWELVE_SUPPRESSED = register("item.aa12.suppressed");
+    public static final RegistryObject<SoundEvent> SPAS12_FIRE = register("item.spas12.fire");
+    public static final RegistryObject<SoundEvent> SPAS12_COCK = register("item.spas12.cock");
+    public static final RegistryObject<SoundEvent> SPAS12_RELOAD = register("item.spas12.reload");
+    public static final RegistryObject<SoundEvent> AA_12_FIRE = register("item.aa12.fire");
+    public static final RegistryObject<SoundEvent> AA_12_COCK = register("item.aa12.cock");
+    public static final RegistryObject<SoundEvent> AA_12_SUPPRESSED = register("item.artic_warfare_magnum.suppressed");
+    //Misc Other Sounds
+    public static final RegistryObject<SoundEvent> MK_170_CROSSBOW_FIRE = register("item.mk_170_crossbow.fire");
+    public static final RegistryObject<SoundEvent> MK_170_CROSSBOW_COCK = register("item.mk_170_crossbow.cock");
+
     private static RegistryObject<SoundEvent> register(String name) {
         return SOUNDS.register(name, () -> new SoundEvent(new ResourceLocation(StabxModernGuns.MOD_ID, name)));
     }
