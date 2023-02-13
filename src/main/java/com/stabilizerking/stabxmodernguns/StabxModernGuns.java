66d66ae7131a10d4f06818c4fc79551d9e1e10f0
Stabilizer360 <stabilizergaming27@gmail.com>
2023-02-13 18:38:22 +0500

    Updated to 1.19.3

--- src/main/java/com/stabilizerking/stabxmodernguns/StabxModernGuns.java
+++ src/main/java/com/stabilizerking/stabxmodernguns/StabxModernGuns.java
@@ -1,14 +1,19 @@
 package com.stabilizerking.stabxmodernguns;
 
+import com.mrcrayfish.guns.item.GunItem;
 import com.stabilizerking.stabxmodernguns.client.ClientHandler;
 import com.stabilizerking.stabxmodernguns.registeration.ModItemRegisteration;
 import com.stabilizerking.stabxmodernguns.registeration.ModSoundRegisteration;
 import com.stabilizerking.stabxmodernguns.registeration.StabxCreativeModeTab;
+import com.stabilizerking.stabxmodernguns.utils.StabxModUtils;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod(StabxModernGuns.MOD_ID)
 public class StabxModernGuns {
@@ -25,19 +30,159 @@ public class StabxModernGuns {
         bus.addListener(this::clientSetup);
     }
 
+
+    //-----------------------In here we Are Registering creative tabs in 1.19.3 also adding items mannually for each tab----------------------//
+
+
+    //----------------------------------------------------Pistols-------------------------------------------------------------------------------//
+
     private void addCreative(CreativeModeTabEvent.BuildContents event) {
         if (event.getTab() == StabxCreativeModeTab.STABX_MODERN_GUNS_GROUP) {
-            event.accept(ModItemRegisteration.FIVE_HUNDRED_MAGNUM_SMITH_AND_WESSON);
-            event.accept(ModItemRegisteration.GLOCK_19_C);
-            event.accept(ModItemRegisteration.DESERT_EAGLE_MARK_14);
-        }
-        if(event.getTab() == StabxCreativeModeTab.STABX_SMGS_GROUP) {
-            event.accept(ModItemRegisteration.MP5_CLASSIC);
-            event.accept(ModItemRegisteration.MP40);
-            event.accept(ModItemRegisteration.TEC9);
-            event.accept(ModItemRegisteration.STRIKER45);
+            List<GunItem> gunItems = new ArrayList<>();
+
+            gunItems.add(ModItemRegisteration.FIVE_HUNDRED_MAGNUM_SMITH_AND_WESSON.get());
+            gunItems.add(ModItemRegisteration.GLOCK_19_C.get());
+            gunItems.add(ModItemRegisteration.DESERT_EAGLE_MARK_14.get());
+
+            for (GunItem gun : gunItems) {
+                event.accept(StabxModUtils.gunWithoutBar(gun));
+            }
         }
 
+        //----------------------------------------------------Sub Machine Guns-------------------------------------------------------------------------------//
+
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_SMGS_GROUP) {
+            List<GunItem> gunItems = new ArrayList<>();
+
+            gunItems.add(ModItemRegisteration.MP5_CLASSIC.get());
+            gunItems.add(ModItemRegisteration.MP40.get());
+            gunItems.add(ModItemRegisteration.TEC9.get());
+            gunItems.add(ModItemRegisteration.STRIKER45.get());
+
+            for (GunItem gun : gunItems) {
+                event.accept(StabxModUtils.gunWithoutBar(gun));
+            }
+        }
+        //----------------------------------------------------Assault Rifles-------------------------------------------------------------------------------//
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_ASSAULT_RIFLES_GROUP) {
+            List<GunItem> gunItems = new ArrayList<>();
+
+            gunItems.add(ModItemRegisteration.M4.get());
+            gunItems.add(ModItemRegisteration.M4A1.get());
+            gunItems.add(ModItemRegisteration.M16.get());
+            gunItems.add(ModItemRegisteration.MK_MOD11.get());
+            gunItems.add(ModItemRegisteration.SCAR_17.get());
+            gunItems.add(ModItemRegisteration.ACR_BUSHMASTER.get());
+            gunItems.add(ModItemRegisteration.G36C_HD.get());
+            gunItems.add(ModItemRegisteration.AK_47.get());
+            gunItems.add(ModItemRegisteration.AKM_762.get());
+            gunItems.add(ModItemRegisteration.AKS_74_UX.get());
+            gunItems.add(ModItemRegisteration.STG44.get());
+            gunItems.add(ModItemRegisteration.HK416.get());
+
+            for (GunItem gun : gunItems) {
+                event.accept(StabxModUtils.gunWithoutBar(gun));
+            }
+        }
+
+        //----------------------------------------------------Rifles-------------------------------------------------------------------------------//
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_RIFLES_GROUP) {
+            List<GunItem> gunItems = new ArrayList<>();
+
+            gunItems.add(ModItemRegisteration.ARISAKA_TYPE_99.get());
+            gunItems.add(ModItemRegisteration.M1_GRANDE.get());
+
+            for (GunItem gun : gunItems) {
+                event.accept(StabxModUtils.gunWithoutBar(gun));
+            }
+        }
+        //-------------------------------------------------Sniper Rifles-------------------------------------------------------------------------------//
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_SNIPER_RIFLES_GROUP) {
+            List<GunItem> gunItems = new ArrayList<>();
+
+            gunItems.add(ModItemRegisteration.BARRETT_M82.get());
+            gunItems.add(ModItemRegisteration.ARTIC_WARFARE_MAGNUM.get());
+
+            for (GunItem gun : gunItems) {
+                event.accept(StabxModUtils.gunWithoutBar(gun));
+            }
+        }
+        //-------------------------------------------------Shotguns-------------------------------------------------------------------------------//
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_SHOTGUNS_GROUP) {
+            List<GunItem> gunItems = new ArrayList<>();
+
+            gunItems.add(ModItemRegisteration.AA_12.get());
+            gunItems.add(ModItemRegisteration.SPAS_12.get());
+
+            for (GunItem gun : gunItems) {
+                event.accept(StabxModUtils.gunWithoutBar(gun));
+            }
+        }
+
+        //-------------------------------------------------Light Machine Guns-------------------------------------------------------------------------------//
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_LMGS_GROUP) {
+            List<GunItem> gunItems = new ArrayList<>();
+
+            gunItems.add(ModItemRegisteration.M60.get());
+
+            for (GunItem gun : gunItems) {
+                event.accept(StabxModUtils.gunWithoutBar(gun));
+            }
+        }
+
+        //----------------------------------------------Miscellaneous-------------------------------------------------------------------------------//
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_MISCELLANEOUS_GROUP) {
+            List<GunItem> gunItems = new ArrayList<>();
+
+            gunItems.add(ModItemRegisteration.MK_170_CROSSBOW.get());
+
+            for (GunItem gun : gunItems) {
+                event.accept(StabxModUtils.gunWithoutBar(gun));
+            }
+
+        }
+        //----------------------------------------------Attachmments-------------------------------------------------------------------------------//
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_ATTACHMENTS_GROUP) {
+            event.accept(ModItemRegisteration.RED_DOT_SIGHT);
+            event.accept(ModItemRegisteration.HOLOGRAPHIC_SIGHT);
+            event.accept(ModItemRegisteration.ACOG_SIGHT);
+            event.accept(ModItemRegisteration.ZA_MINI_RED_DOT_SIGHT);
+            event.accept(ModItemRegisteration.CUSTOM_STOCK);
+            event.accept(ModItemRegisteration.ANGLED_FOREGRIP);
+            event.accept(ModItemRegisteration.VERTICAL_FOREGRIP);
+            event.accept(ModItemRegisteration.ADVANCED_MUZZLE_BRAKE);
+            event.accept(ModItemRegisteration.ADVANCED_SUPPRESSOR);
+        }
+
+        //----------------------------------------------Ammuniation-------------------------------------------------------------------------------//
+
+
+        if (event.getTab() == StabxCreativeModeTab.STABX_AMMUNITION_GROUP) {
+            event.accept(ModItemRegisteration.NINE_MM);
+            event.accept(ModItemRegisteration.NINE_X_19MM_PARABELLUM);
+            event.accept(ModItemRegisteration.SEVEN_62_MM);
+            event.accept(ModItemRegisteration.SEVEN_32_X_51_MM_NATO);
+            event.accept(ModItemRegisteration.TWELVE_GAUGE_BUCKSHOT_SLUG_SHELL);
+            event.accept(ModItemRegisteration.FIVE_56_X_45_NATO);
+            event.accept(ModItemRegisteration.SEVEN_7_X_58_MM);
+            event.accept(ModItemRegisteration.SEVEN_32_X_33_MM_KURZ);
+            event.accept(ModItemRegisteration.FIFTY_CALIBER);
+            event.accept(ModItemRegisteration.FOURTY_FIVE_ACP);
+            event.accept(ModItemRegisteration.THRITY_SIXTY_AMMO);
+            event.accept(ModItemRegisteration.FIFTY_AE);
+            event.accept(ModItemRegisteration.THREE_HUNDRED_WM);
+            event.accept(ModItemRegisteration.FIFTY_BMG);
+            event.accept(ModItemRegisteration.MK_CROSSBOW_ARROW);
+
+        }
     }
 
     private void clientSetup(FMLClientSetupEvent event) {
--- src/main/java/com/stabilizerking/stabxmodernguns/registeration/StabxCreativeModeTab.java
+++ src/main/java/com/stabilizerking/stabxmodernguns/registeration/StabxCreativeModeTab.java
@@ -1,36 +1,97 @@
-package com.stabilizerking.stabxmodernguns.registeration;
+    package com.stabilizerking.stabxmodernguns.registeration;
 
-import com.stabilizerking.stabxmodernguns.StabxModernGuns;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+    import com.stabilizerking.stabxmodernguns.StabxModernGuns;
+    import com.stabilizerking.stabxmodernguns.utils.StabxModUtils;
+    import net.minecraft.network.chat.Component;
+    import net.minecraft.resources.ResourceLocation;
+    import net.minecraft.world.item.CreativeModeTab;
+    import net.minecraft.world.item.ItemStack;
+    import net.minecraftforge.event.CreativeModeTabEvent;
+    import net.minecraftforge.eventbus.api.SubscribeEvent;
+    import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = StabxModernGuns.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class StabxCreativeModeTab {
+    @Mod.EventBusSubscriber(modid = StabxModernGuns.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public class StabxCreativeModeTab {
 
-    public static CreativeModeTab STABX_MODERN_GUNS_GROUP;
-    public static CreativeModeTab STABX_SMGS_GROUP;
+        //----------------------------In here we register every Creative Tab for each gun category-------------------------------------//
+        public static CreativeModeTab
+                STABX_MODERN_GUNS_GROUP,
+                STABX_SMGS_GROUP,
+                STABX_ASSAULT_RIFLES_GROUP,
+                STABX_RIFLES_GROUP,
+                STABX_SNIPER_RIFLES_GROUP,
+                STABX_SHOTGUNS_GROUP,
+                STABX_LMGS_GROUP,
+                STABX_MISCELLANEOUS_GROUP,
+                STABX_ATTACHMENTS_GROUP,
+                STABX_AMMUNITION_GROUP;
 
-    @SubscribeEvent
-    public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
-        STABX_MODERN_GUNS_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabxmodernguns"),
-                builder -> builder.icon(() -> {
-                    ItemStack itemStack = new ItemStack(ModItemRegisteration.FIVE_HUNDRED_MAGNUM_SMITH_AND_WESSON.get());
-                    itemStack.getOrCreateTag().putInt("AmmoCount", ModItemRegisteration.FIVE_HUNDRED_MAGNUM_SMITH_AND_WESSON.get().getGun().getGeneral().getMaxAmmo());
-                    return itemStack;
-                }).title(Component.literal("Stabx Modern Guns")).build());
+        //------------------------------Now the Real Fun Begins in every creative tab uses builder and event thingy -------------------------------------//
 
 
-        STABX_SMGS_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabx_smgs"),
-                builder -> builder.icon(() -> {
-                    ItemStack itemStack = new ItemStack(ModItemRegisteration.MP5_CLASSIC.get());
-                    itemStack.getOrCreateTag().putInt("AmmoCount", ModItemRegisteration.MP5_CLASSIC.get().getGun().getGeneral().getMaxAmmo());
-                    return itemStack;
-                }).title(Component.literal("Sub Machine Guns")).build());
+        //----------------------------------------------------Pistols-------------------------------------------------------------------------------//
+        @SubscribeEvent
+        public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
+            STABX_MODERN_GUNS_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabxmodernguns"),
+                    builder -> builder.icon(() -> StabxModUtils.gunWithoutBar(ModItemRegisteration.FIVE_HUNDRED_MAGNUM_SMITH_AND_WESSON.get())).
+                            title(Component.literal("Stabx Modern Guns")).build());
+
+            //----------------------------------------------------Sub Machine Guns-------------------------------------------------------------------------------//
+
+
+            STABX_SMGS_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabx_smgs"),
+                    builder -> builder.icon(() -> StabxModUtils.gunWithoutBar(ModItemRegisteration.MP5_CLASSIC.get())).
+                            title(Component.literal("Sub Machine Guns")).build());
+
+            //----------------------------------------------------Assault Rifles-------------------------------------------------------------------------------//
+
+            STABX_ASSAULT_RIFLES_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabx_assault_rifles"),
+                    builder -> builder.icon(() -> StabxModUtils.gunWithoutBar(ModItemRegisteration.M4.get())).
+                            title(Component.literal("Assault Rifles")).build());
+
+            //-------------------------------------------------------Rifles-------------------------------------------------------------------------------//
+
+            STABX_RIFLES_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabx_rifles"),
+                    builder -> builder.icon(() -> StabxModUtils.gunWithoutBar(ModItemRegisteration.ARISAKA_TYPE_99.get())).
+                            title(Component.literal("Rifles")).build());
+
+            //----------------------------------------------------Sniper Rifles-------------------------------------------------------------------------------//
+
+
+            STABX_SNIPER_RIFLES_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabx_sniper_rifles"),
+                    builder -> builder.icon(() -> StabxModUtils.gunWithoutBar(ModItemRegisteration.ARTIC_WARFARE_MAGNUM.get())).
+                            title(Component.literal("Sniper Rifles")).build());
+
+            //----------------------------------------------------Light Machine Guns -------------------------------------------------------------------------------//
+
+
+            STABX_LMGS_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabx_lmgs"),
+                    builder -> builder.icon(() -> StabxModUtils.gunWithoutBar(ModItemRegisteration.M60.get())).
+                            title(Component.literal("Light Machine Guns")).build());
+
+            //----------------------------------------------------Shot guns-------------------------------------------------------------------------------//
+
+
+            STABX_SHOTGUNS_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabx_shotguns"),
+                    builder -> builder.icon(() -> StabxModUtils.gunWithoutBar(ModItemRegisteration.AA_12.get())).
+                            title(Component.literal("Shotguns")).build());
+
+
+            //-------------------------------------------------------Miscellaneous-------------------------------------------------------------------------------//
+
+            STABX_MISCELLANEOUS_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID, "stabx_miscellaneous"),
+                    builder -> builder.icon(() -> StabxModUtils.gunWithoutBar(ModItemRegisteration.MK_170_CROSSBOW.get())).
+                            title(Component.literal("Miscellaneous")).build());
+
+            //--------------------------------------------------------Attachements-------------------------------------------------------------------------------//
+
+            STABX_ATTACHMENTS_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID,"stabx_attachments"),
+                    builder -> builder.icon(() -> new ItemStack(ModItemRegisteration.RED_DOT_SIGHT.get())).title(Component.literal("Attachments")).build());
+
+            //---------------------------------------------------------Ammunition-------------------------------------------------------------------------------//
+
+            STABX_AMMUNITION_GROUP = event.registerCreativeModeTab(new ResourceLocation(StabxModernGuns.MOD_ID,"stabx_ammunition"),
+                    builder -> builder.icon(() -> new ItemStack(ModItemRegisteration.NINE_MM.get())).title(Component.literal("Ammunition")).build());
+
+        }
     }
-
-}
